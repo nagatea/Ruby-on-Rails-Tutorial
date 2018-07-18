a313ce83bd61c8434f2a7ae31267987b10d004e7
nagatech <cheesecakebot@yahoo.co.jp>
2018-07-19 03:40:22 +0900

    第3章終了

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file,
   # see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'static_pages#home'
 end
