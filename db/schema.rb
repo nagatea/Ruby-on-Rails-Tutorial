55bed5903a0f6c60ef9581b4ddbc0fba1e284c11
nagatech <cheesecakebot@yahoo.co.jp>
2018-08-12 15:15:27 +0900

    6.1演習まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180708093517) do
+ActiveRecord::Schema.define(version: 20180812053654) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
