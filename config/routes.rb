139ce9a3352091cd812de479c777841bfdf3b757
nagatech <cheesecakebot@yahoo.co.jp>
2018-08-19 20:08:04 +0900

    8.2演習まで

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :microposts
   resources :users
