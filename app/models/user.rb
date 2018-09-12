f84b8dfe0cbddb029f2fa55a5414e897dfdfd466
nagatech <cheesecakebot@yahoo.co.jp>
2018-09-12 18:51:01 +0900

    14.1演習まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   before_save { email.downcase! }
 
@@ -17,5 +21,21 @@ class User < ApplicationRecord
 
   def feed
     Micropost.created_desc.where("user_id = ?", id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  def followed_by?(other_user)
+    followers.include?(other_user)
   end
 end
