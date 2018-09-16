9b82adfaad2d9a3ee01d0fdd095f275c86a37b6e
nagatech <cheesecakebot@yahoo.co.jp>
2018-09-16 23:00:09 +0900

    14.3演習まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,8 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.created_desc.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.created_desc.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   def follow(other_user)
