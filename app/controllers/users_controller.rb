68ffa7bc2766f55166f601830c367ac399958ca4
nagatech <cheesecakebot@yahoo.co.jp>
2018-09-19 16:53:11 +0900

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,14 +50,14 @@ class UsersController < ApplicationController
   def following
     @title = "Following"
     @user  = User.find(params[:id])
-    @users = @user.following.paginate(page: params[:page])
+    @follow_users = @user.following.paginate(page: params[:page])
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
     @user  = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
+    @follow_users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
   end
 
