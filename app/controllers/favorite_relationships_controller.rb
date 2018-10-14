6031e359962add32e83dc257debca8f62745c08e
nagatech <cheesecakebot@yahoo.co.jp>
2018-10-14 16:17:40 +0900

    お気に入り機能

---
+++ app/controllers/favorite_relationships_controller.rb
@@ -0,0 +1,21 @@
+class FavoriteRelationshipsController < ApplicationController
+  before_action :logged_in_user
+  
+  def create
+    @micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite(@micropost)
+    respond_to do |format|
+      format.html {redirect_to root_path }
+      format.js
+    end
+  end
+
+  def destroy
+    @favorite = FavoriteRelationship.find(params[:id]).micropost
+    current_user.unfavorite(@micropost)
+    respond_to do |format|
+      format.html {redirect_to root_path }
+      format.js
+    end
+  end
+end
