0a285dd1f44d1ec0aec20fff0c66b4c90ee09222
nagatech <cheesecakebot@yahoo.co.jp>
2018-10-23 17:32:37 +0900

    WIP

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @user = current_user
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = Micropost.paginate_by_sql(current_user.feed, page: params[:page])
     end
   end
 
